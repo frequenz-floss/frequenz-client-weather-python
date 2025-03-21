a2b73cd20bdbd0def60836301c58302792a5e69b
Talwinder Singh <talwinder.singh@frequenz.com>
2025-03-21 17:07:35 +0100

    Expose Client class in weather package __init__.py
    Signed-off-by: Talwinder Singh <talwinder.singh@frequenz.com>

--- src/frequenz/client/weather/__init__.py
+++ src/frequenz/client/weather/__init__.py
@@ -2,3 +2,8 @@
 # Copyright © 2025 Frequenz Energy-as-a-Service GmbH
 
 """Weather API Client for Python."""
+
+from ._client import Client
+from ._types import ForecastFeature, Forecasts, Location
+
+__all__ = ["Client", "ForecastFeature", "Forecasts", "Location"]
