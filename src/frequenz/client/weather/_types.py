eb4107fdefd2265c4477960a2c0778e5fe9deaac
cwasicki <126617870+cwasicki@users.noreply.github.com>
2025-10-01 19:12:12 +0200

    Require timezone-aware timestamps in forecast upsampling
    Using timezone-naive datetime objects is discouraged. Supporting naive
    timestamps has already caused problems in downstream apps by the
    `timestamp()` method implicitly assuming local time instead of UTC.
    
    Signed-off-by: cwasicki <126617870+cwasicki@users.noreply.github.com>

--- src/frequenz/client/weather/_types.py
+++ src/frequenz/client/weather/_types.py
@@ -396,8 +396,8 @@ class Forecasts:
 
         Args:
             array: 3D array from to_ndarray_vlf (time, location, feature)
-            validity_times: List of original timestamps
-            target_times: List of desired timestamps to interpolate to
+            validity_times: List of original timestamps (tz-aware)
+            target_times: List of desired timestamps to interpolate to (tz-aware)
             features: List of forecast features
             solar_offset_sec: Time offset in seconds to shift solar forecasts
 
@@ -422,6 +422,13 @@ class Forecasts:
         # Validate target timestamps are strictly increasing
         if not all(t1 < t2 for t1, t2 in zip(target_times[:-1], target_times[1:])):
             raise ValueError("target_times must be strictly increasing")
+
+        # We require tz-aware datetime objects to avoid ambiguity,
+        # since for naive timestamps local time is assumed.
+        if any(t.tzinfo is None or t.utcoffset() is None for t in target_times):
+            raise ValueError("All target_times must be timezone-aware")
+        if any(t.tzinfo is None or t.utcoffset() is None for t in validity_times):
+            raise ValueError("All validity_times must be timezone-aware")
 
         vts = np.array([t.timestamp() for t in validity_times])
         tts = np.array([t.timestamp() for t in target_times])
